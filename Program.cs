b8e3210a8a2fefe92a5a04d4551b10fc8789c589
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-03-10 23:18:20 +0530

    [Muskan] Add : C# program to Compare the two lines according to input points and check which one is longer.

--- Program.cs
+++ Program.cs
@@ -12,8 +12,28 @@ namespace LineComparison
         {
             Console.WriteLine(" Welcome to line comparison computation program.");
 
-            //LineLength.CalcLengthofLine();
-            LineEquality.CalcEqualityofLine();
+            LineLength.CalcLengthofLine();
+
+
+
+            Console.WriteLine("Please provide the (x,y) co-ordinates of the first end point of Line1 : ");
+            double x1_1 = Convert.ToInt32(Console.ReadLine());
+            double y1_1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please provide the (x,y) co-ordinates of the second end point of Line1 : ");
+            double x2_1 = Convert.ToInt32(Console.ReadLine());
+            double y2_1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please provide the (x,y) co-ordinates of the first end point of Line2 : ");
+            double x1_2 = Convert.ToInt32(Console.ReadLine());
+            double y1_2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please provide the (x,y) co-ordinates of the second end point of Line2 : ");
+            double x2_2 = Convert.ToInt32(Console.ReadLine());
+            double y2_2 = Convert.ToInt32(Console.ReadLine());
+
+            LineEquality.CalcEqualityofLine(x1_1, y1_1, x2_1, y2_1, x1_2, y1_2, x2_2, y2_2);
+            LineComparison.ComparisonOfLine(x1_1, y1_1, x2_1, y2_1, x1_2, y1_2, x2_2, y2_2);
         }
     }
 }
