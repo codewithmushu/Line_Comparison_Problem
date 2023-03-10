dd4dcf1bba661d7c38235997a3fe816877c21677
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-03-10 23:28:42 +0530

    [Muskan] Add : added new features.

--- LineEquality.cs
+++ LineEquality.cs
@@ -8,23 +8,9 @@ namespace LineComparison
 {
      public class LineEquality
     {
-        public static void CalcEqualityofLine()
+        public static void CalcEqualityofLine(double x1_1, double y1_1, double x2_1, double y2_1, double x1_2, double y1_2, double x2_2, double y2_2)
         {
-            Console.WriteLine("Please provide the (x,y) co-ordinates of the first end point of Line1 : ");
-            double x1_1 = Convert.ToInt32(Console.ReadLine());
-            double y1_1 = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Please provide the (x,y) co-ordinates of the second end point of Line1 : ");
-            double x2_1 = Convert.ToInt32(Console.ReadLine());
-            double y2_1 = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Please provide the (x,y) co-ordinates of the first end point of Line2 : ");
-            double x1_2 = Convert.ToInt32(Console.ReadLine());
-            double y1_2 = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Please provide the (x,y) co-ordinates of the second end point of Line2 : ");
-            double x2_2 = Convert.ToInt32(Console.ReadLine());
-            double y2_2 = Convert.ToInt32(Console.ReadLine());
+            
 
             if ((x1_1 == x1_2 &&  y1_1 == y1_2 && x2_1 == x2_2 && y2_1 == y2_2) ||
                     (x1_1 == x2_2 && y1_1 == y2_2 && x2_1 == x1_2 && y2_1 == y1_2))
